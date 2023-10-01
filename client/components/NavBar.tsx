652f5cdbc9a3c22dbf1b0ca7356e4c8fec2d289d
yeauxdejuan <dejuan.ells@gmail.com>
2023-09-30 20:18:24 -0500

    add: MUI menuBar

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,32 +1,117 @@
-// NavBar.tsx
-import React from 'react';
+import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import { FormControlLabel } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 
-//temp in-line styling until bootstrap global 
-const navBarStyle: React.CSSProperties = {
-  backgroundColor: 'blue',
-  overflow: 'hidden'
-};
-//temp in-line styling until bootstrap global
-const linkStyle: React.CSSProperties = {
-  float: 'left',
-  display: 'block',
-  color: 'yellow',
-  textAlign: 'center',
-  padding: '14px 16px',
-  textDecoration: 'none'
-};
+
 const NavBar = () => {
+
+  //authenticate user
+  const [auth, setAuth] = React.useState<boolean | null>(true);
+  //set anchor to display or close menu in navBar 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  //handler for setting auth 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  //handler to menu in navBar
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  //handler to close menu
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+
   return (
-    <nav style={navBarStyle}>
-      <ul>
-      <Link to="/journey" style={linkStyle}>Journey</Link>
-      <Link to="/profile" style={linkStyle}>Profile</Link>
-      <Link to="/leaderboard" style={linkStyle}>Leaderboard</Link>
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      {/* <FormGroup>
+        <FormControlLabel
+          control={
+            <Routes
+              checked={auth}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup> */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem component={Link} to="/profile"  onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Journey</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+ 
+    
   );
 }
 
 export default NavBar;
+/**
+ * 
+ *  <ul>
+      <Link to="/journey" >Journey</Link>
+      <Link to="/profile" >Profile</Link>
+      <Link to="/leaderboard" >Leaderboard</Link>
+      </ul>
+ */
