d5ff7720e7f963c266b4f9efbfd7977b374d4360
yeauxdejuan <dejuan.ells@gmail.com>
2023-10-03 15:23:05 -0500

    update: user crud passing, put pending

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -4,10 +4,10 @@ import AppDataSource from '../db';
 
 //create repo instance for User entity from AppDataSource
 const userRepo = AppDataSource.getRepository(User);
-const Users = express.Router();
+const userRouter = express.Router();
 
-//GET User by :id
-Users.get('/:id', async (req, res) => {
+//GET User by :id (tested: √ )
+userRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const user = await userRepo.findOneBy( {id: parseInt(id)  } );
@@ -20,24 +20,24 @@ Users.get('/:id', async (req, res) => {
   }
 });
 
-//GET User by username
-Users.get('/username/:username', async (req, res) => {
+//GET User by username (tested: √ )
+userRouter.get('/username/:username', async (req, res) => {
   const { username } = req.params;
   try {
-    const user = await userRepo.findOneBy( {username: username } );
+    const user = await userRepo.findOne( { where : {username: username } } );
     if (user) {
       res.status(200).send(user)
     } else {
       res.status(404).send('User not found in database')
     }
   } catch(err) {
-    console.error(err);
+    console.error('Database Error', err);
     res.status(500).send('Internal Server Error')
   }
 });
 
-//GET all users
-Users.get('/', async (req, res) => {
+//GET all users (tested: √ )
+userRouter.get('/', async (req, res) => {
   try {
     const users = await userRepo.find();
     res.status(200).send(users);
@@ -47,18 +47,37 @@ Users.get('/', async (req, res) => {
   }
 });
 
-//POST new user
-Users.post('/', async (req, res) => {
+//POST new user (tested: √ )
+userRouter.post('/', async (req, res) => {
   const { username, img_url } = req.body;
   try {
     const newUser = userRepo.create({ username, img_url });
     await userRepo.save(newUser);
-    res.status(200).send(newUser);
+    res.status(201).send(newUser);
   } catch(err) {
     console.error(err);
     res.sendStatus(500);
   }
 });
 
+//Put to adjust user photo (tested: 404)
+userRouter.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { img_url } = req.body;
 
-export default Users;
+  try {
+    const user = await userRepo.findOneBy( {id: parseInt(id)  } );
+    if (!user) {
+      res.status(404).send('User not found' )
+    }
+    user.img_url = img_url;
+    await userRepo.save(user);
+    res.status(200).send(user);
+  } catch(err) {
+    console.error('Error updating photo', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+export default userRouter;
