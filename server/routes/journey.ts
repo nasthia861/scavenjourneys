a04481d948ef954a25e35b90594146bca87408ff
nasthia861 <nasthia861@gmail.com>
2023-10-04 16:10:49 -0500

    fixed ts errors;

--- server/routes/journey.ts
+++ server/routes/journey.ts
@@ -11,8 +11,8 @@ const journeyRepository = AppDataSource.getRepository(Journey);
 //get all journeys
 journeyRouter.get('/', async(req, res) => {
   journeyRepository.find()
-    .then((journeys) => res.status(200).json(journeys))
-    .catch((error) => {
+    .then((journeys: []) => res.status(200).json(journeys))
+    .catch((error: null) => {
       console.error('could not get all journeys', error);
       res.status(500);
   })
@@ -21,10 +21,10 @@ journeyRouter.get('/', async(req, res) => {
 //get journeys by tag
 journeyRouter.get('/tag/:id', async(req, res) => {
   const { id } = req.params;
-  AppDataSource.manager.findOneBy(Journey, {
+  AppDataSource.manager.findBy(Journey, {
     tagId: +id,
   })
-    .then((journeys) => {
+    .then((journeys: []) => {
       console.log(journeys);
       if(journeys) {
         res.status(200).send(journeys)
@@ -33,7 +33,7 @@ journeyRouter.get('/tag/:id', async(req, res) => {
         res.status(404)
       }
     })
-    .catch((error) => {
+    .catch((error: null) => {
       console.error('could not get journeys by tag', error);
       res.status(500);
   })
@@ -44,8 +44,8 @@ journeyRouter.post('/', async (req, res) => {
   const newJourneyData = req.body; // Assuming the request body contains journey data
   const newJourney = journeyRepository.create(newJourneyData);
   await journeyRepository.save(newJourney)
-    .then((createdJourney) => res.status(201).json(createdJourney))
-    .catch((error) => {
+    .then((createdJourney: {}) => res.status(201).json(createdJourney))
+    .catch((error: null) => {
       console.error('could not create journey', error);
       res.status(500);
     });
@@ -55,15 +55,15 @@ journeyRouter.post('/', async (req, res) => {
 journeyRouter.put('/:id', async (req, res) => {
   const journeyId = req.params.id;
   const updatedJourneyData = req.body; // Assuming the request body contains updated journey data
-  await journeyRepository.update(journeyId, updatedJourneyData)
-    .then((result) => {
-      if (result.affected > 0) {
+  await AppDataSource.manager.update(Journey, {id: journeyId}, updatedJourneyData)
+    .then((value) => {
+      if (value.affected > 0) {
         res.status(200).json({ message: 'Journey updated successfully' });
       } else {
         res.status(404).json({ message: 'Journey not found' });
       }
     })
-    .catch((error) => {
+    .catch((error: null) => {
       console.error('could not update journey', error);
       res.status(500);
     });
@@ -72,7 +72,7 @@ journeyRouter.put('/:id', async (req, res) => {
 // delete a journey by id
 journeyRouter.delete('/:id', async (req, res) => {
   const journeyId = req.params.id;
-  await journeyRepository.delete(journeyId)
+  await AppDataSource.manager.delete(Journey, {id: journeyId})
     .then((result) => {
       if (result.affected > 0) {
         res.status(200).json({ message: 'Journey deleted successfully' });
@@ -80,7 +80,7 @@ journeyRouter.delete('/:id', async (req, res) => {
         res.status(404).json({ message: 'Journey not found' });
       }
     })
-    .catch((error) => {
+    .catch((error: null) => {
       console.error('could not delete journey', error);
       res.status(500);
     });
