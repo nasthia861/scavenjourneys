c225a001776ba70760f69f87b75ff76c98b7c1ff
nasthia861 <nasthia861@gmail.com>
2023-10-04 20:14:55 -0500

    journeys rendering with search

--- client/components/Search.tsx
+++ client/components/Search.tsx
@@ -1,73 +1,54 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, ChangeEvent} from 'react';
 import axios from 'axios';
-import InputBase from '@mui/material/InputBase';
-import { styled, alpha} from '@mui/material/styles';
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(0)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
-const SearchBase = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(10),
-    width: 'auto',
-  },
-}));
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 const Search = () => {
-  const [searchInput, setSearchInput] = useState('')
+  const [searchInput, setSearchInput] = useState(''); 
   const [journeys, setJourneys] = useState([]);
 
   const getJourney = () => {
-    axios.get(`/journey/name/${searchInput}`)
-      .then((journey: {}) => {
-        setJourneys([journey])
+    axios.get(`journey/name/${searchInput}`)
+      .then((journey: {data: {}}) => {
+        setJourneys([journey.data])
       })
   }
 
-  const handleChange = (e: React.ChangeEvent) => {
-    e.preventDefault();
-    setSearchInput(e.target.textContent);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.currentTarget.value);
   };
 
-  useEffect(() => {
-
-  }, [journeys])
 
   return (
     <div>
-      <SearchBase>
-            <StyledInputBase
-              placeholder="Journey Search"
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={handleChange}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                getJourney();
-                }
-              }}
-              value={searchInput}
-              />
-      </SearchBase>
+      <input type="text"
+        placeholder='Search Journey'
+        onChange={handleChange}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            getJourney();
+          }
+        }}
+        value={searchInput}/>
+      <section>
+        <ImageList sx={{ width: 500, height: 450 }}>
+        {journeys.map((journey: {id: number, name: string, img_url: string, description: string, userId: number}) => (
+          <ImageListItem key={journey.id}>
+            <img
+              src={journey.img_url}
+              alt={journey.description}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={journey.name}
+              subtitle={<span>by: {journey.userId}</span>}
+              position="below"
+            />
+          </ImageListItem>
+        ))}
+        </ImageList>
+      </section>
     </div>
   )
 }
