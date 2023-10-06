076a2021710da5b31e80b8ec69e3185b45ca5441
yeauxdejuan <dejuan.ells@gmail.com>
2023-10-06 14:51:37 -0500

    update: journey => home

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { Button, Container, Grid, Card, TextField, CardContent, CardMedia, Typography } from '@mui/material';
 
-import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
 import Search from './Search'
 import JourneyItem from './JourneyItem';
-import { Journey } from '@this/types/Journey';
+import { User } from '@this/types/User';
 
 const StyledCreateJourneyButton = styled(Button)(() => ({
   backgroundColor: '#9a4119', // Change the background color
@@ -18,7 +18,18 @@ const StyledCreateJourneyButton = styled(Button)(() => ({
 }));
 
 const Home: React.FC = () => {
-  const [journeys, setJourneys] = useState<Journey[]>([]);
+
+ //set user state to User or null
+ const [user, setUser] = useState<User | null>(null);
+
+ const [journeys, setJourneys] = useState([]);
+ const [journeyId, setJourneyId] = useState('')
+ const [newJourneyName, setNewJourneyName] = useState('');
+ const [selectedJourney, setSelectedJourney] = useState(null);
+ const [steps, setSteps] = useState([]);
+
+
+  // const [journeys, setJourneys] = useState<Journey[]>([]);
 
   useEffect(() => {
     // Fetch the most recently made 20 journeys
@@ -31,9 +42,51 @@ const Home: React.FC = () => {
       });
   }, []);
 
+  useEffect(() => {
+    // get steps for the selected journey
+    if (selectedJourney) {
+      axios.get(`/step/journey/${selectedJourney.id}`)
+        .then((stepAndJourney) => {
+          //console.log(stepAndJourney)
+          setSteps(stepAndJourney.data);
+        })
+        .catch((error) => {
+          console.error('Error getting steps for journey:', error);
+        });
+    }
+  }, [selectedJourney]);
+
+  //assign Journey to User
+  // const assignJourney = () => {
+  // if (user && selectedJourney) {
+  //   axios.post(`/journey/assign/${selectedJourney.id}`, { userId: user.id })
+  //     .then((response) => {
+  //       console.log('Journey assigned to user:', response.data);
+  //       setSuccessMessage('Journey assigned successfully!');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error assigning journey to user:', error);
+  //       setSuccessMessage('');
+  //     });
+  // }
+  // };
+
 return (
   <Container>
     <Search />
+
+     {/* Styled "Create a New Journey" button */}
+     <Link to="/create-journey">
+        <StyledCreateJourneyButton variant="contained">Create a New Journey</StyledCreateJourneyButton>
+      </Link>
+
+
+      {/* Render the list of recently made journeys */}
+      <div className="journey-list">
+        {journeys.map((journey) => (
+          <JourneyItem key={journey.id} journey={journey} />
+        ))}
+      </div>
     <h1> Journey Begins Here! *change me*</h1>
     <Grid container spacing={2}>
       {/* Display list of journeys */}
@@ -65,34 +118,24 @@ return (
         </Grid>
       ))}
     </Grid>
+      {/* Display selected journey details steps */}
+      {selectedJourney && (
+        <div>
+          <h2>Selected Journey: {selectedJourney.name}</h2>
+          <p>Description: {selectedJourney.description}</p>
 
-    {/* Display selected journey details w/steps */}
-    {selectedJourney && (
-      <div>
-      {/* Styled "Create a New Journey" button */}
-      <Link to="/create-journey">
-        <StyledCreateJourneyButton variant="contained">Create a New Journey</StyledCreateJourneyButton>
-      </Link>
+          <h3>Steps:</h3>
+          {steps.map((step) => (
+            <div key={step.id}>
+              <p>Details: {step.name}</p>
+              <p>Location: {step.location.latitude}, {step.location.longitude}</p>
+            </div>
+          ))}
+        </div>
+      )}
 
-         <h2>Selected Journey: {selectedJourney.name}</h2>
-        <p>Description: {selectedJourney.description}</p>
 
-        <h3>Steps:</h3>
-        {steps.map((step) => (
-          <div key={step.id}>
-            <p>Details: {step.name}</p>
-            <p>Location: {step.location.latitude}, {step.location.longitude}</p>
-          </div>
-        ))}
 
-      {/* Render the list of recently made journeys */}
-      <div className="journey-list">
-        {journeys.map((journey) => (
-          <JourneyItem key={journey.id} journey={journey} />
-        ))}
-      </div>
-      </div>
-    )}
   </Container>
 );
 ;
--- client/components/Journey.tsx
+++ client/components/Journey.tsx
@@ -1,130 +1,130 @@
-// import React, {useState, useEffect} from 'react';
-// import { Button, Container, Grid, Card, TextField, CardContent, CardMedia, Typography } from '@mui/material';
-// import axios from 'axios';
-// //import { Step } from '../types/Step';
+import React, {useState, useEffect} from 'react';
+import { Button, Container, Grid, Card, TextField, CardContent, CardMedia, Typography } from '@mui/material';
+import axios from 'axios';
+//import { Step } from '../types/Step';
 
-// import { User } from '../types/User';
-
-
-// const Journey = () => {
-
-//   //set user state to User or null
-//   const [user, setUser] = useState<User | null>(null);
-
-//   const [journeys, setJourneys] = useState([]);
-//   const [journeyId, setJourneyId] = useState('')
-//   const [newJourneyName, setNewJourneyName] = useState('');
-//   const [selectedJourney, setSelectedJourney] = useState(null);
-//   const [steps, setSteps] = useState([]);
-//   const [successMessage, setSuccessMessage] = useState('');
+import { User } from '../types/User';
 
 
+const Journey = () => {
 
-//   useEffect(() => {
-//     // Fetch existing
-//     axios.get('/journey')
-//       .then((journeys) => {
-//         setJourneys(journeys.data);
-//         //console.log(journeys.data)
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching journeys:', error);
-//       });
+  //set user state to User or null
+  const [user, setUser] = useState<User | null>(null);
+
+  const [journeys, setJourneys] = useState([]);
+  const [journeyId, setJourneyId] = useState('')
+  const [newJourneyName, setNewJourneyName] = useState('');
+  const [selectedJourney, setSelectedJourney] = useState(null);
+  const [steps, setSteps] = useState([]);
+  const [successMessage, setSuccessMessage] = useState('');
 
 
-//   }, []);
 
-//   useEffect(() => {
-//     // get steps for the selected journey
-//     if (selectedJourney) {
-//       axios.get(`/step/journey/${selectedJourney.id}`)
-//         .then((stepAndJourney) => {
-//           //console.log(stepAndJourney)
-//           setSteps(stepAndJourney.data);
-//         })
-//         .catch((error) => {
-//           console.error('Error getting steps for journey:', error);
-//         });
-//     }
-//   }, [selectedJourney]);
+  useEffect(() => {
+    // Fetch existing
+    axios.get('/journey')
+      .then((journeys) => {
+        setJourneys(journeys.data);
+        //console.log(journeys.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching journeys:', error);
+      });
 
-//   // //Not in complete working state Logan, placeholder
-//   // const createJourney = () => {
-//   //   axios.post('/journey', { name: newJourneyName })
-//   //     .then((response) => {
-//   //       console.log('Journey created successfully:', response.data);
 
-//   //     })
-//   //     .catch((error) => {
-//   //       console.error('Error creating journey:', error);
-//   //     });
-//   // };
+  }, []);
 
-//   // const assignJourney = () => {
-//   // if (user && selectedJourney) {
-//   //   axios.post(`/journey/assign/${selectedJourney.id}`, { userId: user.id })
-//   //     .then((response) => {
-//   //       console.log('Journey assigned to user:', response.data);
-//   //       setSuccessMessage('Journey assigned successfully!');
-//   //     })
-//   //     .catch((error) => {
-//   //       console.error('Error assigning journey to user:', error);
-//   //       setSuccessMessage('');
-//   //     });
-//   // }
-//   // };
+  useEffect(() => {
+    // get steps for the selected journey
+    if (selectedJourney) {
+      axios.get(`/step/journey/${selectedJourney.id}`)
+        .then((stepAndJourney) => {
+          //console.log(stepAndJourney)
+          setSteps(stepAndJourney.data);
+        })
+        .catch((error) => {
+          console.error('Error getting steps for journey:', error);
+        });
+    }
+  }, [selectedJourney]);
 
-//   return (
-//     <Container>
-//       <h1> Journey Begins Here!</h1>
-//       <Grid container spacing={2}>
-//         {/* Display list of journeys */}
-//         {journeys.map((journey) => (
-//           <Grid item key={journey.id} xs={12} sm={6} md={4}>
-//             <Card >
-//               <CardMedia
-//                 component="img"
-//                 alt={journey.name}
-//                 height="140"
-//                 image={journey.img_url}
-//               />
-//               <CardContent>
-//                 <Typography variant="h6" component="div">
-//                   {journey.name}
-//                   <br/>
-//                   {journey.description}
-//                 </Typography>
-//                 <Button
-//                   variant="contained"
-//                   color="primary"
-//                   onClick={() => setSelectedJourney(journey)}
-//                   style={{ marginTop: 10 }}
-//                 >
-//                   More Details
-//                 </Button>
-//               </CardContent>
-//             </Card>
-//           </Grid>
-//         ))}
-//       </Grid>
+  // //Not in complete working state Logan, placeholder
+  // const createJourney = () => {
+  //   axios.post('/journey', { name: newJourneyName })
+  //     .then((response) => {
+  //       console.log('Journey created successfully:', response.data);
 
-//       {/* Display selected journey details steps */}
-//       {selectedJourney && (
-//         <div>
-//           <h2>Selected Journey: {selectedJourney.name}</h2>
-//           <p>Description: {selectedJourney.description}</p>
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error creating journey:', error);
+  //     });
+  // };
 
-//           <h3>Steps:</h3>
-//           {steps.map((step) => (
-//             <div key={step.id}>
-//               <p>Details: {step.name}</p>
-//               <p>Location: {step.location.latitude}, {step.location.longitude}</p>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </Container>
-//   );
-// };
+  // const assignJourney = () => {
+  // if (user && selectedJourney) {
+  //   axios.post(`/journey/assign/${selectedJourney.id}`, { userId: user.id })
+  //     .then((response) => {
+  //       console.log('Journey assigned to user:', response.data);
+  //       setSuccessMessage('Journey assigned successfully!');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error assigning journey to user:', error);
+  //       setSuccessMessage('');
+  //     });
+  // }
+  // };
 
-// export default Journey;
+  return (
+    <Container>
+      <h1> Journey Begins Here!</h1>
+      <Grid container spacing={2}>
+        {/* Display list of journeys */}
+        {journeys.map((journey) => (
+          <Grid item key={journey.id} xs={12} sm={6} md={4}>
+            <Card >
+              <CardMedia
+                component="img"
+                alt={journey.name}
+                height="140"
+                image={journey.img_url}
+              />
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {journey.name}
+                  <br/>
+                  {journey.description}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => setSelectedJourney(journey)}
+                  style={{ marginTop: 10 }}
+                >
+                  More Details
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      {/* Display selected journey details steps */}
+      {selectedJourney && (
+        <div>
+          <h2>Selected Journey: {selectedJourney.name}</h2>
+          <p>Description: {selectedJourney.description}</p>
+
+          <h3>Steps:</h3>
+          {steps.map((step) => (
+            <div key={step.id}>
+              <p>Details: {step.name}</p>
+              <p>Location: {step.location.latitude}, {step.location.longitude}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default Journey;
