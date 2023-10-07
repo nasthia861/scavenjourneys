5f42279c9c9c474da90ee60be29e6a93d8a19bb4
nasthia861 <nasthia861@gmail.com>
2023-10-07 10:58:18 -0500

    user location seed changes

--- server/db/seeding/factories/journey.factory.ts
+++ server/db/seeding/factories/journey.factory.ts
@@ -5,9 +5,9 @@ export default setSeederFactory(Journey, (faker) => {
     const journey = new Journey();
     journey.name = faker.company.buzzNoun();
     journey.description = faker.lorem.sentence();
-    journey.location = { 
-      latitude: faker.location.latitude(),
-      longitude: faker.location.longitude()
+    journey.location = {
+      latitude: faker.location.latitude(30.03526000, 29.19678),
+      longitude: faker.location.longitude(-90.03517586, -90.15188)
     };
     journey.tagId = faker.number.int({min:1, max: 5})
     journey.img_url= faker.image.url();
